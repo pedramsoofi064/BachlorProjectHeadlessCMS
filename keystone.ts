300e23bcd2938f4b52c1704360c3b1dd5710b960
pedram soufi <pedram.soufi@part.local>
2021-11-13 14:29:24 +0330

    add server

--- keystone.ts
+++ keystone.ts
@@ -1,30 +1,29 @@
+import { config } from "@keystone-next/keystone";
+import { statelessSessions } from "@keystone-next/keystone/session";
+import { createAuth } from "@keystone-next/auth";
 
-import { config } from '@keystone-next/keystone';
-import { statelessSessions } from '@keystone-next/keystone/session';
-import { createAuth } from '@keystone-next/auth';
-
-import { lists } from './schema';
-
-
+import { lists } from "./schema";
 
 const { withAuth } = createAuth({
-  listKey: 'User',
-  identityField: 'email',
-  secretField: 'password',
-  sessionData: 'email',
+  listKey: "User",
+  identityField: "email",
+  secretField: "password",
+  sessionData: "email",
   initFirstItem: {
-    fields: ['email', 'password'],
+    fields: ["email", "password"],
     itemData: { isAdmin: true },
   },
 });
 
+let sessionSecret = 'process.enfdfsdfsssssssssssssssssssssssssssssssssssv.SESSION_SECRET';
 
-let sessionSecret = process.env.SESSION_SECRET;
 if (!sessionSecret) {
-  if (process.env.NODE_ENV === 'production') {
-    throw new Error('The SESSION_SECRET environment variable must be set in production');
+  if (process.env.NODE_ENV === "production") {
+    throw new Error(
+      "The SESSION_SECRET environment variable must be set in production"
+    );
   } else {
-    sessionSecret = '--x DEV COOKIE SECRET; CHANGE ME x--';
+    sessionSecret = "--x DEV COOKIE SECRET; CHANGE ME x--";
   }
 }
 
@@ -41,21 +40,26 @@ export default withAuth(
   config({
     // the db sets the database provider - we're using sqlite for the fastest startup experience
     db: {
-      provider: 'sqlite',
-      url: 'file:./keystone.db',
+      provider: "sqlite",
+      url: "file:./keystone.db",
     },
     // This config allows us to set up features of the Admin UI https://keystonejs.com/docs/apis/config#ui
     ui: {
       // For our starter, we check that someone has session data before letting them see the Admin UI.
       isAccessAllowed: (context) => !!context.session?.data,
     },
+    server: {
+      port: 3000,
+      maxFileSize: 200 * 1024 * 1024,
+      healthCheck: true,
+    },
     lists,
     session,
     images: {
-      upload: 'local',
+      upload: "local",
       local: {
-        storagePath: 'public/uploads/images',
-        baseUrl: '/images',
+        storagePath: "public/uploads/images",
+        baseUrl: "/images",
       },
     },
   })
